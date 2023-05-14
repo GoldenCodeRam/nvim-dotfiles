79e1c6ae7b602877cbac8472e2840504f4d15606
Luis Alejandro Quiroga Gómez <lualquigo@gmail.com>
2023-05-14 18:33:27 -0500

    fix: merge files

---
+++ lua/goldencoderam/lazy.lua
@@ -0,0 +1,196 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- example using a list of specs with the default options
+vim.g.mapleader = " " -- make sure to set `mapleader` before lazy so your mappings are correct
+
+require("lazy").setup({
+    {
+        "rebelot/kanagawa.nvim",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("kanagawa").setup({
+                transparent = true,
+            })
+            -- Lua
+            vim.cmd([[colorscheme kanagawa]])
+        end,
+    },
+    {
+        -- This I feel is needed, as I sometimes forget the keys. This might
+        -- be disabled later, but I like it.
+        "folke/which-key.nvim",
+        lazy = true,
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            -- disable netrw at the very start of your init.lua (strongly advised)
+            vim.g.loaded_netrw = 1
+            vim.g.loaded_netrwPlugin = 1
+
+            -- set termguicolors to enable highlight groups
+            vim.opt.termguicolors = true
+
+            -- empty setup using defaults
+            require("nvim-tree").setup()
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-context",
+        config = function()
+            require("treesitter-context").setup({
+                enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+                max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+                min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+                line_numbers = true,
+                multiline_threshold = 20, -- Maximum number of lines to collapse for a single context line
+                trim_scope = "outer", -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+                mode = "cursor", -- Line used to calculate context. Choices: 'cursor', 'topline'
+                -- Separator between context and content. Should be a single character string, like '-'.
+                -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+                separator = nil,
+                zindex = 20, -- The Z-index of the context window
+            })
+        end,
+    },
+    {
+        "mbbill/undotree",
+        lazy = false,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter",
+        lazy = false,
+        build = function()
+            require("nvim-treesitter.install").update({ with_sync = true })
+        end,
+        config = function()
+            require("nvim-treesitter.configs").setup({
+                auto_install = true,
+                highlight = {
+                    enable = true,
+                },
+            })
+        end,
+    },
+    {
+        "windwp/nvim-autopairs",
+        lazy = false,
+        config = function()
+            require("nvim-autopairs").setup({})
+        end,
+    },
+    {
+        "tpope/vim-fugitive",
+    },
+    {
+        -- LSP configuration
+        "neovim/nvim-lspconfig",
+        lazy = true,
+        dependencies = {
+            -- LSP support
+            "williamboman/mason.nvim",
+            "williamboman/mason-lspconfig.nvim",
+        },
+    },
+    {
+        -- Autocompletion
+        "hrsh7th/nvim-cmp",
+        lazy = false,
+        dependencies = {
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            -- Snippets
+            --"hrsh7th/cmp-vsnip",
+            --"hrsh7th/vim-vsnip",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+        },
+    },
+    {
+        "jose-elias-alvarez/null-ls.nvim",
+        lazy = true,
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+    },
+    {
+        "jay-babu/mason-null-ls.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "williamboman/mason.nvim",
+            "jose-elias-alvarez/null-ls.nvim",
+        },
+    },
+    {
+        "folke/trouble.nvim",
+        lazy = true,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("trouble").setup({})
+        end,
+    },
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.1",
+        -- or                              , branch = '0.1.1',
+        dependencies = { "nvim-lua/plenary.nvim" },
+        lazy = true,
+    },
+    {
+        -- Notifications, pretty cool.
+        "folke/noice.nvim",
+        lazy = false,
+        config = function()
+            -- Suggested setup.
+            require("noice").setup({
+                lsp = {
+                    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                        ["cmp.entry.get_documentation"] = true,
+                    },
+                },
+                -- you can enable a preset for easier configuration
+                presets = {
+                    bottom_search = true,         -- use a classic bottom cmdline for search
+                    command_palette = true,       -- position the cmdline and popupmenu together
+                    long_message_to_split = true, -- long messages will be sent to a split
+                    inc_rename = false,           -- enables an input dialog for inc-rename.nvim
+                    lsp_doc_border = false,       -- add a border to hover docs and signature help
+                },
+            })
+        end,
+        dependencies = {
+            "MunifTanjim/nui.nvim",
+            -- This one is optional.
+            -- "rcarriga/nvim-notify",
+        },
+    },
+    {
+        "nvim-lualine/lualine.nvim",
+        lazy = false,
+        config = function()
+            require("lualine").setup({})
+        end,
+    },
+})
---
+++ lua/goldencoderam/set.lua
@@ -0,0 +1,39 @@
+vim.opt.guicursor = ""
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+-- tabbing
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+
+-- show matching brackets
+vim.opt.showmatch = true
+
+-- disable text wrap
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+-- Disable this thing...
+vim.g.omni_sql_no_default_maps = 1
